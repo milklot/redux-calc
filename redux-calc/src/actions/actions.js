8d0bfa4b034ac788f7d7d6cdeec30f355f3700b1
milklot <vbiostflex@gmail.com>
2021-12-30 21:25:26 -0500

    add actions

---
+++ redux-calc/src/actions/actions.js
@@ -0,0 +1,7 @@
+export const ADD = "ADD";
+export const SUBTRACT = "SUBSTRACT";
+export const MULTIPLY = "MULTIPLY";
+export const DIVIDE = "DIVIDE";
+export const CLEAR = "CLEAR";
+export const EQUAL = "EQUAL";
+export const RESET = "RESET";
